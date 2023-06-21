5ea0f470dcf8a50d3fd1e14d341ca20836b13734
MR-Addict <2750417853@qq.com>
2023-06-21 11:44:51 +0800

    styles: make home page suitable for mobile device

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,19 +11,19 @@ export default async function Page() {
     <main className="w-full px-4 md:px-48 py-10 flex-1 flex flex-col items-center justify-center">
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6 md:gap-10">
         <Link href="/form" className={style.card}>
-          <FcAddressBook size={100} />
+          <FcAddressBook className={style.icon} />
           <h1 className={style.title}>提交笔记</h1>
           <p className={style.subtitle}>记录今天的值班内容</p>
         </Link>
 
         <Link href="/view" className={style.card}>
-          <FcDataSheet size={100} />
+          <FcDataSheet className={style.icon} />
           <h1 className={style.title}>查看笔记</h1>
           <p className={style.subtitle}>去看本学期的值班笔记</p>
         </Link>
 
         <Link href="/archive" className={style.card}>
-          <FcKindle size={100} />
+          <FcKindle className={style.icon} />
           <h1 className={style.title}>笔记归档</h1>
           <p className={style.subtitle}>去看学长学姐的值班笔记</p>
         </Link>
