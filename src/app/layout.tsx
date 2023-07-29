62c6d9ac15d6d4d362006582b288ab55a5cc82ab
MR-Addict <2750417853@qq.com>
2023-07-29 12:33:32 +0800

    restore: remove umami analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import "./globals.css";
 
-import Script from "next/script";
 import { Footer, Navbar } from "@/components";
 import { PopupContextProvider } from "@/contexts";
 
@@ -14,7 +13,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           <Footer />
         </PopupContextProvider>
       </body>
-      <Script async src="https://umami.mraddict.one/script.js" data-website-id="0a767ab9-b602-47dd-bab4-485ca6e9179b" />
     </html>
   );
 }
--- src/lib/utils/groupBy.ts
+++ src/lib/utils/groupBy.ts
@@ -1,8 +1,11 @@
 export default function groupBy<T>(array: T[], predicate: (value: T, index: number, array: T[]) => string) {
-  const groups = array.reduce((acc, value, index, array) => {
-    (acc[predicate(value, index, array)] ||= []).push(value);
-    return acc;
-  }, {} as { [key: string]: T[] });
+  const groups = array.reduce(
+    (acc, value, index, array) => {
+      (acc[predicate(value, index, array)] ||= []).push(value);
+      return acc;
+    },
+    {} as { [key: string]: T[] }
+  );
 
   return Object.keys(groups).map((category) => ({
     category,
