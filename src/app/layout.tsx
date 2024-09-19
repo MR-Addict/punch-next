3e17b99accef6561cdd455bb1cd1bcc4297cc95e
Cael <MR-Addict@qq.com>
2024-09-19 13:09:18 +0800

    fix: remove telegram webapp

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,21 +5,18 @@ import BottomNavbar from "@/components/Navbar/BottomNavbar/BottomNavbar";
 
 import { AppContextProvider } from "@/contexts/App/AppProvider";
 import { PopupContextProvider } from "@/contexts/Popup/PopupProvider";
-import { TelegramWebAppContextProvider } from "@/contexts/TelegramWebApp/TelegramWebAppProvider";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="zh-Hans">
       <body>
-        <TelegramWebAppContextProvider>
-          <AppContextProvider>
-            <PopupContextProvider>
-              <TopNavbar />
-              {children}
-              <BottomNavbar />
-            </PopupContextProvider>
-          </AppContextProvider>
-        </TelegramWebAppContextProvider>
+        <AppContextProvider>
+          <PopupContextProvider>
+            <TopNavbar />
+            {children}
+            <BottomNavbar />
+          </PopupContextProvider>
+        </AppContextProvider>
       </body>
     </html>
   );
--- src/components/Navbar/BottomNavbar/BottomNavbar.tsx
+++ src/components/Navbar/BottomNavbar/BottomNavbar.tsx
@@ -1,21 +1,18 @@
 "use client";
 
-import clsx from "clsx";
 import Link from "next/link";
 import { motion } from "framer-motion";
 import { usePathname } from "next/navigation";
 
 import links from "../config";
 import style from "./BottomNavbar.module.css";
-import { useTelegramWebAppContext } from "@/contexts/TelegramWebApp/TelegramWebAppProvider";
 
 export default function BottomNavbar() {
-  const { webApp } = useTelegramWebAppContext();
   const rootPath = (usePathname() || "/").split("/").slice(0, 2).join("/");
 
   return (
     <nav className="w-full sticky bottom-0 bg-purple-50 md:hidden">
-      <ul className={clsx(style.links, { [style["tg-web-app"]]: webApp?.initData })}>
+      <ul className={style.links}>
         {links.map((link) => (
           <li key={link.name}>
             <Link href={link.link} className={style.link}>
