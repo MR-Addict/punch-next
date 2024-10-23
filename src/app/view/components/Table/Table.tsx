33d271fb08ef78798bf69befecb3dbc5dfc40c4e
Cael <MR-Addict@qq.com>
2024-10-23 15:38:32 +0800

    feat: allow toggle between interval and date two date formats

--- src/app/view/components/Table/Table.tsx
+++ src/app/view/components/Table/Table.tsx
@@ -5,12 +5,15 @@ import { AiOutlineCalendar, AiOutlineUser } from "react-icons/ai";
 import style from "./Table.module.css";
 import groupBy from "@/lib/utils/groupBy";
 import formatDate from "@/lib/utils/formatDate";
+import timeInterval from "@/lib/utils/timeInterval";
+import usePersistantState from "@/hooks/usePersistantState";
 import { useTableContext } from "@/contexts/Table/TableProvider";
 
 import MarkdownRenderer from "@/components/MarkdownRenderer/MarkdownRenderer";
 
 export default function Table() {
   const { currentNotes } = useTableContext();
+  const [intervalFomatDate, setIntervalFomatDate] = usePersistantState("view-interval-format-date", true);
 
   if (currentNotes.length === 0) return <h1 className="w-full text-center py-28 rounded-sm">没有符合条件的结果</h1>;
 
@@ -28,12 +31,21 @@ export default function Table() {
                 <p className={style.avatar}>{note.name.at(0)}</p>
 
                 <div className="flex flex-col w-full gradient-50 py-4 px-5 rounded-md shadow-md duration-300">
-                  <h2 className="flex flex-row items-center gap-0.5 border-b border-b-gray-300 w-fit text-gray-500">
-                    <AiOutlineUser />
-                    <span className="mr-2">{note.name}</span>
+                  <h2 className="flex flex-row gap-2 w-fit text-gray-500">
+                    <p className="flex flex-row items-center gap-0.5 border-b border-b-gray-300">
+                      <AiOutlineUser />
+                      <span className="mr-2">{note.name}</span>
+                    </p>
 
-                    <AiOutlineCalendar />
-                    <span>{formatDate(note.date)}</span>
+                    <button
+                      type="button"
+                      className="flex flex-row items-center gap-0.5 border-b border-b-gray-300"
+                      onClick={() => setIntervalFomatDate((prev) => !prev)}
+                    >
+                      <AiOutlineCalendar />
+                      {intervalFomatDate && <span>{timeInterval(note.date)}</span>}
+                      {!intervalFomatDate && <span>{formatDate(note.date, false)}</span>}
+                    </button>
                   </h2>
 
                   {new Date(note.date).getFullYear() >= 2024 ? (
--- src/contexts/Table/TableProvider.tsx
+++ src/contexts/Table/TableProvider.tsx
@@ -4,7 +4,7 @@ import { createContext, useContext, useState, useMemo, useEffect } from "react";
 
 import { NoteDatabseType } from "@/types/notes";
 
-const notesPerpage = 20;
+const notesPerpage = 50;
 
 interface TableContextProps {
   totalPages: number;
