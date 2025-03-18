d784bd12a1540aa9e422a837e79e34357fce998f
Cael <MR-Addict@qq.com>
2025-03-18 12:46:20 +0800

    feat: update ai

--- src/lib/ai/llm-ner/llm-ner.ts
+++ src/lib/ai/llm-ner/llm-ner.ts
@@ -3,74 +3,41 @@ import { generateObject } from "ai";
 import { ollama } from "ollama-ai-provider";
 import "dotenv/config";
 
-import { prompt } from "./consts";
 import getArchivedNotes from "@/lib/notes/getArchivedNotes";
 import getArchivedTerms from "@/lib/notes/getArchivedTerms";
 
-const model = ollama("gemma3");
+const model = ollama("gemma3:7b");
 const terms = getArchivedTerms();
 
 async function LLMNER(text: string) {
-  try {
-    const { object } = await generateObject({
-      model: model,
-      output: "array",
-      schema: z.string(),
-      prompt: prompt.ner(text)
-    });
-    return object;
-  } catch (err) {
-    return [];
-  }
-}
+  const { object } = await generateObject({
+    model: model,
+    output: "array",
+    schema: z.string().describe("The named entity recognized by the model"),
+    prompt: `You are the expert in recognizing meaningful and useful named entities from given text.
 
-async function LLMRemoveDuplicates(text: string) {
-  try {
-    const { object } = await generateObject({
-      model: model,
-      output: "array",
-      schema: z.string(),
-      prompt: prompt.removeDuplicates(text)
-    });
-    return object;
-  } catch (err) {
-    return [];
-  }
-}
+    Please read the following text and recognize the named entities:
 
-async function LLMNormalize(text: string) {
-  try {
-    const { object } = await generateObject({
-      model: model,
-      output: "array",
-      schema: z.string(),
-      prompt: prompt.normalize(text)
-    });
-    return object;
-  } catch (err) {
-    return [];
-  }
+    ${text}
+    `
+  });
+  return object;
 }
 
 (async () => {
-  const res = getArchivedNotes(terms[4].name, 1, 1000, "");
+  const term = terms[4].name;
+  const res = getArchivedNotes(term, 1, 1000, "");
   if (!res.success) return;
 
-  const ners = await new Promise<string[]>((resolve) => {
+  await new Promise<string[]>((resolve) => {
     let finished = 0;
     const ners: string[] = [];
-    res.data.data.forEach(async (note, index) => {
+    res.data.data.forEach(async (note) => {
       const data = await LLMNER(note.content);
-      console.log(`Note[${note._id}]: ${data.join(", ")}`);
+      console.log(`Note[${note._id}](${finished + 1}/${res.data.pagination.total}): ${data.join(", ")}`);
       ners.push(...data);
       finished++;
-      if (finished === res.data.data.length) resolve(ners);
+      if (finished === res.data.pagination.total) resolve(ners);
     });
   });
-
-  const nersArray = Array.from(new Set(ners));
-  const uniqueNers = await LLMRemoveDuplicates(nersArray.join(", "));
-
-  const normalizedNers = await LLMNormalize(uniqueNers.join(", "));
-  console.log(normalizedNers);
 })();
